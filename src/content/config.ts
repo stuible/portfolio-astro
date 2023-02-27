0cadbae2f2d3c9361a738f63d267ef8f205b7b79
Josh Stuible <joshstuible@gmail.com>
2023-02-27 11:22:42 -0800

    Subtitles

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ import { z, defineCollection } from 'astro:content';
 const projectCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    subtitle: z.string(),
     // tags: z.array(z.string()),
   }),
 });
