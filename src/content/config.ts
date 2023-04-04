d57b3eba125936dd973608a8e708e93b33ac51dd
Josh Stuible <joshstuible@gmail.com>
2023-04-03 17:21:44 -0700

    Add tree for stack

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,24 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
+import { type TreeNode } from '../components/project/TreeNode.astro';
 // 2. Define a schema for each collection you'd like to validate.
+
+const treeNodeSchema: z.ZodSchema<TreeNode[]> = z.lazy(() =>
+  z.array(
+    z.object({
+      name: z.string(),
+      children: treeNodeSchema.optional(),
+    })
+  )
+);
+
+
 const projectCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     subtitle: z.string(),
     tags: z.array(z.string()).optional(),
+    stack: treeNodeSchema.optional(),
   }),
 });
 // 3. Export a single `collections` object to register your collection(s)
