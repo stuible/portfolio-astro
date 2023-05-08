81e760b92fa8429f305f84936bb7db5a8d9d5e37
Josh Stuible <joshstuible@gmail.com>
2023-05-07 23:34:59 -0700

    Move treeNode to types files

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
-import { type TreeNode } from '../components/project/TreeNode.astro';
+import { type TreeNode } from '~/types';
 // 2. Define a schema for each collection you'd like to validate.
 
 const treeNodeSchema: z.ZodSchema<TreeNode[]> = z.lazy(() =>
@@ -20,6 +20,7 @@ const projectCollection = defineCollection({
     subtitle: z.string(),
     tags: z.array(z.string()).optional(),
     stack: treeNodeSchema.optional(),
+    featured: z.boolean(),
   }),
 });
 // 3. Export a single `collections` object to register your collection(s)
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type TreeNode = {
+    name: string;
+    url?: string;
+    children?: TreeNode[];
+};
