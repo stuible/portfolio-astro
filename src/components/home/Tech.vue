f5f7111bf4ea061213e789e10a162ecf371a7c5d
Josh Stuible <joshstuible@gmail.com>
2024-04-28 02:08:51 -0700

    IconPill is now .vue, and create a vue wrapper for the Tech to reduce number of vue instances

---
+++ src/components/home/Tech.vue
@@ -0,0 +1,58 @@
+<template>
+    <ol class="tech">
+        <li v-for="(item, index) in techWithIcons">
+            <IconPill :label="item.name" :icon="item.icon" :tooltip="item.tooltip" :colour="item.colour"
+                :id="`tech-${index}`" />
+        </li>
+    </ol>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import IconPill from '../global/IconPill.vue';
+
+const props = defineProps<{
+    techWithIcons: {
+        name: string;
+        icon: string;
+        tooltip: string;
+        colour: string;
+    }[]
+}>();
+
+
+</script>
+
+<style scoped lang="scss">
+.tech {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.4rem;
+
+    @include breakpoint(sm) {
+        gap: 0.5rem;
+    }
+
+    @include breakpoint(md) {
+        gap: 0.6rem;
+    }
+
+    @include breakpoint(lg) {
+        gap: 0.7rem;
+    }
+
+    li {
+        display: flex;
+        gap: 1rem;
+    }
+
+    .logo {
+        height: 1em;
+    }
+
+    h3 {
+        font-size: 0.7rem;
+        font-weight: normal;
+    }
+}
+</style>
